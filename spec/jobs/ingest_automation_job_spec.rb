4b7070c67719e72c1261731f3d9e97e03de771d8
Jie Chen <jadychen@umich.edu>
2026-06-18 09:41:28 -0500

    debug the IngestAutomationJob Spec

--- spec/jobs/ingest_automation_job_spec.rb
+++ spec/jobs/ingest_automation_job_spec.rb
@@ -5,18 +5,18 @@ require 'rails_helper'
 RSpec.describe IngestAutomationJob, type: :job do
   describe 'queue' do
     it 'is enqueued on the default queue' do
-      expect(described_class.new.queue_name).to eq('default')
+      expect(described_class.new("event", {}).queue_name).to eq('automation')
     end
 
     it 'enqueues the job' do
-      expect { described_class.perform_later }
-        .to have_enqueued_job(described_class).on_queue('default')
+      expect { described_class.perform_later("event", {}) }
+        .to have_enqueued_job(described_class).on_queue('automation')
     end
   end
 
   describe '#perform' do
     it 'runs without raising an error' do
-      expect { described_class.perform_now }.not_to raise_error
+      expect { described_class.perform_now("event", {}) }.not_to raise_error
     end
   end
 end
