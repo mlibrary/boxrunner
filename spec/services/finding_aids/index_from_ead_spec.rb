db43ba7e123122974633fd7a449cd42e93b3fc57
Jie Chen <jadychen@umich.edu>
2026-06-19 15:05:19 -0500

    modify the index_from_ead_spec.rb to pass the test

--- spec/services/finding_aids/index_from_ead_spec.rb
+++ spec/services/finding_aids/index_from_ead_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe FindingAids::IndexFromEad do
 
   before do
     stub_const('Box::IndexError', Class.new(StandardError))
-    allow(IngestAutomationJob).to receive(:perform_later)
   end
 
   context 'when source open fails' do
@@ -14,17 +13,9 @@ RSpec.describe FindingAids::IndexFromEad do
       allow(File).to receive(:open).with(src_path, 'r:UTF-8:UTF-8').and_raise(StandardError, 'boom')
     end
 
-    it 'enqueues index.failure and raises Box::IndexError' do
+    it 'raises Box::IndexError' do
       expect { described_class.call(src_path, repo_id) }
         .to raise_error(Box::IndexError, 'boom')
-
-      expect(IngestAutomationJob).to have_received(:perform_later).with(
-        'index.failure',
-        src_path: src_path,
-        archive_path: nil,
-        ead_id: nil,
-        err_msg: 'boom'
-      )
     end
   end
 end
